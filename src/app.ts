09ca7d5942b527430cce79eb281cba1e365a80e9
nennosleep <vanntrong7890@gmail.com>
2026-07-15 12:07:46 +0700

    chore(challenge_03): merge challenge 2 customer api into challenge 3

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './config/swagger';
-import { healthCheck } from './modules/health/health.controller';
+import healthRouter from './modules/health/health.route';
 import authRoute from './modules/auth/auth.route';
+import customerRoute from './modules/customer/customer.route';
+import userRoute from './modules/user/user.route';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
 app.use(express.json());
-app.use(
-  '/api-docs',
-  swaggerUi.serve as any,
-  swaggerUi.setup(swaggerSpec) as any,
-);
-app.use('/api', healthCheck);
+app.use('/api-docs', swaggerUi.serve as any, swaggerUi.setup(swaggerSpec) as any);
+app.use('/api', healthRouter);
 app.use('/api', authRoute);
+app.use('/api', customerRoute);
+app.use('/api', userRoute);
 app.use(errorHandler);
 
 export default app;
