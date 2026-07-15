8e8d9ad799287d51eb4f6f244859e93a3d2aabfd
nennosleep <vanntrong7890@gmail.com>
2026-07-15 12:07:38 +0700

    fix(challenge_02): ignore build distribution folder

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './config/swagger';
-import healthRoute from './modules/health/health.route';
+import healthRouter from './modules/health/health.route';
 import customerRoute from './modules/customer/customer.route';
 import userRoute from './modules/user/user.route';
 import { errorHandler } from './middlewares/errorHandler';
@@ -9,15 +9,10 @@ import { errorHandler } from './middlewares/errorHandler';
 const app = express();
 
 app.use(express.json());
-app.use(
-  '/api-docs',
-  swaggerUi.serve as any,
-  swaggerUi.setup(swaggerSpec) as any,
-);
-app.use('/api', healthRoute);
+app.use('/api-docs', swaggerUi.serve as any, swaggerUi.setup(swaggerSpec) as any);
+app.use('/api', healthRouter);
 app.use('/api', customerRoute);
 app.use('/api', userRoute);
-
 app.use(errorHandler);
 
 export default app;
